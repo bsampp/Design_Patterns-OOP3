ee7f71d8dd0baefe5de8fc7338d76fdad32469cf
bsampp <b.sampietro02@gmail.com>
2023-06-17 16:58:57 -0300

    git test

--- Design Patterns/01-FactoryMethod/src/transport/vehicles/Car.ts
+++ Design Patterns/01-FactoryMethod/src/transport/vehicles/Car.ts
@@ -3,7 +3,7 @@ import IVehicle from "./interface/IVehicle";
 export default class Car implements IVehicle{
     startRoute(): void {
         this.getCargo();
-        console.log("Carro: Iniciando o trajeto...");
+        console.log("Carro: Iniciando o trajeto..");
     }
     getCargo(): void {
         console.log("Carro: Passageiros embarcados!");
